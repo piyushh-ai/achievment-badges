8f9e39b3735b635457527f30747524f5d95b1029
Sourav Giri <162682852+softwaredeveloper111@users.noreply.github.com>
2026-08-17 18:01:32 +0530

    add two routes

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+
+app.get("/about",(req,res)=>{
+  res.send("this is about page")
+})
+
+
+app.get("/home",(req,res)=>{
+  res.send("home page")
+})
+
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
